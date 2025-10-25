3bae9c0a50ad211bb3d11030db442a2377946e6b
Ira Horecka <ira89@icloud.com>
2025-10-25 15:03:28 -0400

    update imports

--- src/risk/neighborhoods/stats/__init__.py
+++ src/risk/neighborhoods/stats/__init__.py
@@ -3,8 +3,8 @@ risk/_neighborhoods/_stats
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-from ._permutation import compute_permutation_test
-from ._tests import (
+from .permutation import compute_permutation_test
+from .tests import (
     compute_binom_test,
     compute_chi2_test,
     compute_hypergeom_test,
